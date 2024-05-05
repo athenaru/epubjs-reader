16cdb9302197d7064515725177b30a3e12103e0b
Athena Ru <athena2021191@gmail.com>
2024-05-05 00:24:39 -0400

    moved file upload button

--- reader/js/input.js
+++ reader/js/input.js
@@ -1,7 +1,7 @@
 // var book = ePub();
-var rendition;
+// var rendition;
 
-var inputElement = document.getElementById("input");
+var inputElement = document.getElementById("input-path");
 
 inputElement.addEventListener('change', function (e) {
       var file = e.target.files[0];
@@ -20,48 +20,48 @@ function openBook(e, fileName){
   var title = document.getElementById("title");
   var next = document.getElementById("next");
   var prev = document.getElementById("prev");
-  
+
   window.reader = ePubReader(bookPath, {
     restore: true
 });
 
-//   book.open(bookData, "binary");
+//   book.open(bookData, "epub");
 
-  rendition = window.reader.renderTo("viewer", {
-    width: "100%",
-    height: 600
-  });
+//   rendition = book.renderTo("viewer", {
+//     width: "100%",
+//     height: 600
+//   });
 
-  rendition.display();
+//   rendition.display();
   
-  var keyListener = function(e){
+//   var keyListener = function(e){
 
-    // Left Key
-    if ((e.keyCode || e.which) == 37) {
-      rendition.prev();
-    }
+//     // Left Key
+//     if ((e.keyCode || e.which) == 37) {
+//       rendition.prev();
+//     }
 
-    // Right Key
-    if ((e.keyCode || e.which) == 39) {
-      rendition.next();
-    }
+//     // Right Key
+//     if ((e.keyCode || e.which) == 39) {
+//       rendition.next();
+//     }
 
-  };
+//   };
 
-  rendition.on("keyup", keyListener);
-  rendition.on("relocated", function(location){
-    console.log(location);
-  });
+//   rendition.on("keyup", keyListener);
+//   rendition.on("relocated", function(location){
+//     console.log(location);
+//   });
 
-  next.addEventListener("click", function(e){
-    rendition.next();
-    e.preventDefault();
-  }, false);
+//   next.addEventListener("click", function(e){
+//     rendition.next();
+//     e.preventDefault();
+//   }, false);
 
-  prev.addEventListener("click", function(e){
-    rendition.prev();
-    e.preventDefault();
-  }, false);
+//   prev.addEventListener("click", function(e){
+//     rendition.prev();
+//     e.preventDefault();
+//   }, false);
 
-  document.addEventListener("keyup", keyListener, false);
+//   document.addEventListener("keyup", keyListener, false);
 }
